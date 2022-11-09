3f52448db5bacaebb70f3ee8989e91c20d791f67
FruitDude222 <fabian.gaugusch@gmx.at>
2022-11-09 21:00:41 +0100

    dependencies: added prettier setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <div>
-            <h1>jukebox.party</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>jukebox.party</h1>
+    </div>
+  );
 }
