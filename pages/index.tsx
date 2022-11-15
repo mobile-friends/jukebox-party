9cc59587162024ae4814c9627da9163c34c00fce
FruitDude222 <fabian.gaugusch@gmx.at>
2022-11-15 18:35:57 +0100

    update/ added first draft of db config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
+import db from '../firebase.config';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [starCount, setStarCount] = useState(0);
+  const [userCount, setUserCount] = useState(0);
+
+  useEffect(() => {
+    console.log(db);
+    var starCountRef = db.ref('users/');
+    starCountRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      setStarCount(data);
+    });
+    console.log(starCountRef);
+  }, []);
+
+  const increaseStarCount = (userId) => {
+    db.ref('users/' + userId).set({
+      username: 'test',
+      email: 'fa',
+    });
+    setUserCount(userCount + 1);
+  };
+
   return (
     <div>
       <h1>jukebox.party</h1>
+      <p>{JSON.stringify(starCount)}</p>
+
+      <button onClick={() => increaseStarCount(userCount)}>Increase</button>
     </div>
   );
 }
