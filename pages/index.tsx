e93dca68f46860b431abab3e80357b395a1ebb3b
Eva <evamaria.altenburger@gmail.com>
2022-11-16 15:11:48 +0100

    feat: add file structure with empty btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Button from "../components/elements/button";
+
 export default function Home() {
   return (
     <div>
       <h1>jukebox.party</h1>
+      <Button></Button>
     </div>
   );
 }
