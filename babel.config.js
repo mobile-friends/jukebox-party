72485787902482d6a31be35f09509f5bf68e0ad3
Eva <evamaria.altenburger@gmail.com>
2022-11-16 14:23:18 +0100

    fix: jsx not enabled error
    changed babel config to include @babel/preset-react

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  presets: [
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
 };
