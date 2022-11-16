f5c1e8d6bd3c0ec7122a27690d695848eb450564
lukasganster <lukasganster13@gmail.com>
2022-11-16 19:39:23 +0100

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
-import Button from "../components/elements/button";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+import Button from '../components/elements/button';
 
 export default function Home() {
+  const clientId = process.env.SPOTIFY_CLIENT_ID || 'nope';
+  const session = false;
+
   return (
     <div>
       <h1>jukebox.party</h1>
-      <Button></Button>
+
+      {!session && (
+        <>
+          <h1>You are not signed in</h1> <br />
+          <Button></Button>
+          {clientId}
+        </>
+      )}
+
+      {session && (
+        <>
+          <h1>Signed in as {session?.user?.email} </h1> <br />
+          <h2>
+            Go to{' '}
+            <Link href='/jobs'>
+              <a>Test</a>
+            </Link>{' '}
+          </h2>
+        </>
+      )}
     </div>
   );
 }
