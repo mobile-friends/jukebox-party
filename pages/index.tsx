753a7c10259a9ebc5b8c774362d62a53e3ea0700
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2022-11-17 09:24:25 +0100

    feat: add design for login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import Button from "../components/elements/button";
+import Input from "../components/elements/input";
 
 export default function Home() {
   return (
-    <div>
-      <h1>jukebox.party</h1>
-      <Button></Button>
+    <div style={{
+      height: '100vh',
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
+      <h1 className="text-center">jukebox.<span className="text-primary text-italic">party</span></h1>
+
+      <form>
+        <Input placeholder="Name"></Input>
+        <Input placeholder="Session id"></Input>
+        <Button text="Join session" type="primary"></Button>
+      </form>
+
+      <Button text="Create session" type="tertiary"></Button>
     </div>
   );
 }
