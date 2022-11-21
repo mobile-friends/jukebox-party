9d607c7329f5d6f7facae6013bd8d2a226783146
Ramon Brullo <kingvanti650@gmail.com>
2022-11-21 11:44:06 +0100

    feat: add more domain types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,13 +1,29 @@
-declare type Name = string
-
 declare type Url = string
 
+declare type Id = number
+
+declare type PartyCode = string
+
+declare type UserName = string
+
 declare interface Artist {
-    name: Name
+    name: string
 }
 
 declare interface Track {
-    name: Name,
+    name: string,
     artists: Artist[]
     albumArtUrl: Url
+}
+
+declare interface User {
+    id: Id,
+    name: UserName
+}
+
+declare interface Party {
+    name: string,
+    code: PartyCode,
+    host: User,
+    guests: User[]
 }
