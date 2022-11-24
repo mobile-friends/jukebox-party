d72aaa85f1f1994c261ef77eaff9179986c86b84
ComradeVanti <kingvanti650@gmail.com>
2022-11-24 09:29:03 +0100

    doc: document artist module

--- lib/artist.ts
+++ lib/artist.ts
@@ -1,12 +1,30 @@
+/**
+ * Represents an artist that worked on a track
+ * @see Track
+ */
 export interface Artist {
+  /**
+   * The artists name
+   */
   name: string;
 }
 
+/**
+ * Contains functions for working with artists
+ */
 export namespace Artist {
+  /**
+   * Makes an artist object
+   * @param name The artists name
+   */
   export function make(name: string): Artist {
     return { name };
   }
 
+  /**
+   * Gets the name of an artist
+   * @param artist The artist
+   */
   export function nameOf(artist: Artist): string {
     return artist.name;
   }
