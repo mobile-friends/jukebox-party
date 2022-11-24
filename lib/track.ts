2e45a5c996d9cbe751a41177cb624a44ebb457fe
ComradeVanti <kingvanti650@gmail.com>
2022-11-24 09:40:35 +0100

    doc: document track module

--- lib/track.ts
+++ lib/track.ts
@@ -1,13 +1,25 @@
 import { Artist } from './artist';
 import { Duration } from './duration';
 
+/**
+ * A track that can be played
+ */
 export interface Track {
   readonly name: string;
   readonly duration: Duration;
   readonly artists: Artist[];
 }
 
+/**
+ * Contains functions for working with tracks
+ */
 export namespace Track {
+  /**
+   * Makes a new track
+   * @param name The tracks name
+   * @param duration The tracks duration
+   * @param artists The tracks artists
+   */
   export function make(
     name: string,
     duration: Duration,
@@ -16,14 +28,26 @@ export namespace Track {
     return Object.freeze({ name, duration, artists });
   }
 
+  /**
+   * Gets the name of a track
+   * @param track The track
+   */
   export function nameOf(track: Track): string {
     return track.name;
   }
 
+  /**
+   * Gets the artists of a track
+   * @param track The track
+   */
   export function artistsOf(track: Track): Artist[] {
     return track.artists;
   }
 
+  /**
+   * Gets the duration of a track
+   * @param track The track
+   */
   export function durationOf(track: Track): Duration {
     return track.duration;
   }
