8f792b6b36ae156a791287e1bf0e45bf8de5ca41
FruitDude222 <fabian.gaugusch@gmx.at>
2022-11-25 12:48:41 +0100

    style: JUKE-9/updated component props

--- components/elements/input.tsx
+++ components/elements/input.tsx
@@ -1,7 +1,24 @@
-export default function Input({ placeholder }) {
-    return(
-        <div>
-            <input placeholder={`${placeholder}`} className="block"></input>
-        </div>
-    )
+interface InputProps {
+  type?: string;
+  placeholder: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Input({
+  type,
+  placeholder,
+  onChange,
+}: InputProps): JSX.Element {
+  return (
+    <div>
+      <input
+        type={type}
+        placeholder={`${placeholder}`}
+        className='block'
+        onChange={(e) => {
+          onChange(e);
+        }}
+      ></input>
+    </div>
+  );
 }
