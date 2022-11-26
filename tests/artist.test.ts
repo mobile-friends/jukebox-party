4e626bdb073c60db6c012fc713fc09ef92025cb2
ComradeVanti <kingvanti650@gmail.com>
2022-11-26 19:43:43 +0100

    refactor: remove describe blocks from tests
    They are not needed here

--- tests/artist.test.ts
+++ tests/artist.test.ts
@@ -1,9 +1,7 @@
 import { Artist } from '../lib/artist';
 
-describe('Artist module', () => {
-  test('Created artists have the given name', () => {
-    const name = 'Mr. music';
-    const artist = Artist.make(name);
-    expect(Artist.nameOf(artist)).toBe(name);
-  });
+test('Created artists have the given name', () => {
+  const name = 'Mr. music';
+  const artist = Artist.make(name);
+  expect(Artist.nameOf(artist)).toBe(name);
 });
