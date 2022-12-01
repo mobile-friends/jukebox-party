fe65c2400759ffdb177cad55269d6342be40daf7
Ramon Brullo <kingvanti650@gmail.com>
2022-12-01 10:37:16 +0100

    refactor: drop unused file

--- utils/parties.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase';
-
-export function doesPartyExist(
-  document: firebase.database.Reference
-): Promise<boolean> {
-  return document.get().then((snapshot) => snapshot.exists());
-}
