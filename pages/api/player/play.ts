523ba10b383934b0cb615e6359ef1ce3db211641
Ramon Brullo <kingvanti650@gmail.com>
2022-12-01 09:59:41 +0100

    refactor: player party api-endpoint

--- pages/api/player/play.ts
+++ pages/api/player/play.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getSession, useSession } from 'next-auth/react';
 import { spotifyClient } from '../../../httpClient/spotify';
 import { BaseURL } from './index';
 
@@ -7,12 +6,15 @@ export default async function playTrack(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'PUT') {
-    let spotifyRes = await spotifyClient.get(`${BaseURL}/play`, {
-      headers: {
-        Authorization: req.headers.authorization,
-      },
-    });
-    res.status(200).json(spotifyRes.data);
-  } else res.status(405);
+  if (req.method !== 'PUT') {
+    res.status(405).json({ message: 'Method not allowed' });
+    return;
+  }
+
+  let spotifyRes = await spotifyClient.get(`${BaseURL}/play`, {
+    headers: {
+      Authorization: req.headers.authorization,
+    },
+  });
+  res.status(200).json(spotifyRes.data);
 }
