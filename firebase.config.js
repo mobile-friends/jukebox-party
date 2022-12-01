e664db2d116b55b1e6df2e9b337118d9c21bf12c
Eva <evamaria.altenburger@gmail.com>
2022-12-01 13:49:47 +0100

    fix: firebase config import

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from '@firebase/app';
+import '@firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
