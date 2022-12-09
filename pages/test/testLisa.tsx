39f884d9f6ceb4a419e57f89f4d32cafac20bdfd
lisaMariaX <88780000+lisaMariaX@users.noreply.github.com>
2022-12-09 17:59:20 +0100

    refactor: #JUKE81 errorHandling

--- pages/test/testLisa.tsx
+++ pages/test/testLisa.tsx
@@ -37,37 +37,46 @@ export default function Home() {
   }, [session]);
 
   const getCurrentlyPlaying = async () => {
-    const result = await currentlyPlaying(session?.user?.accessToken);
-    if (result) {
-      const track = createTrack(result.item);
-      setCurrentTrack(track);
-    } else {
-      console.log('no Track is currently playing! Getting Recommendation!');
-      getRecentlyPlayed();
+    try {
+      const result = await currentlyPlaying(session?.user?.accessToken);
+      if (result) {
+        const track = createTrack(result.item);
+        setCurrentTrack(track);
+      } else {
+        console.log('no Track is currently playing! Getting Recommendation!');
+        getRecentlyPlayed();
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   const getRecentlyPlayed = async () => {
-    const results = await recentlyPlayed(session?.user?.accessToken);
-    const seeds = createSeeds(results);
-    const recommendation = await recommendations(
-      //TODO: not working with array, only working with one id
-      seeds.artists.substring(0, seeds.artists.indexOf(',')),
-      seeds.tracks.substring(0, seeds.tracks.indexOf(',')),
-      session?.user?.accessToken
-    );
-    console.log(recommendation.tracks[0]);
+    try {
+      const results = await recentlyPlayed(session?.user?.accessToken);
+      const seeds = createSeeds(results);
+      const recommendation = await recommendations(
+        //TODO: not working with more ids, only working with one id
+        seeds.artists.substring(0, seeds.artists.indexOf(',')),
+        seeds.tracks.substring(0, seeds.tracks.indexOf(',')),
+        session?.user?.accessToken
+      );
+      console.log(recommendation.tracks[0]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getPlaybackState = async () => {
-    await playbackState(session?.user?.accessToken)
-      .then((result) => {
-        const progressDuration: Duration = Duration.makeFromMiliSeconds(
-          result.progress_ms
-        );
-        setPlaybackProgress(progressDuration);
-      })
-      .catch((error) => console.log(error));
+    try {
+      const result = await playbackState(session?.user?.accessToken);
+      const progressDuration: Duration = Duration.makeFromMiliSeconds(
+        result.progress_ms
+      );
+      setPlaybackProgress(progressDuration);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
