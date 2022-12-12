aac91ab718b294190b4c1d77c4c97e4a9e986b11
Eva <evamaria.altenburger@gmail.com>
2022-12-12 11:05:42 +0100

    feat: juke-34 - spotify queue request

--- pages/party/queue.tsx
+++ pages/party/queue.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { tryQueryParam } from '../../lib/query';
 import { useSession } from 'next-auth/react';
-//import { currentQueue } from '../../httpClient/spotify/player';
+import { currentQueue } from '../../httpClient/spotify/player';
 
 type Props = {};
 
@@ -12,12 +12,28 @@ function Queue({}: Props) {
   //   const [currentTrack, setCurrentTrack] = useState<Track>();
 
 
-
   //   const partyCodeParam = tryQueryParam(router.query, 'code');
   //   if (partyCodeParam === null) {
   //     // TODO: Handle missing query param error
   //     throw new Error('Missing query param');
   //   }
+
+  useEffect(() => {
+    getCurrentQueue();
+  }, []);
+
+  const getCurrentQueue = async () => {
+    try {
+      const result = await currentQueue(session?.user?.accessToken);
+      if (result) {
+        console.log(result);
+      } else {
+        console.log('no Track is currently playing! Getting Recommendation!');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return <div>Queue Template</div>;
 }
