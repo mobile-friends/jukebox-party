37035dadb0f8add6ab8c4a5a4f98452375fa25d5
Eva <evamaria.altenburger@gmail.com>
2022-12-14 19:55:51 +0100

    feat: juke-34 styling

--- components/elements/queueHeader.tsx
+++ components/elements/queueHeader.tsx
@@ -10,10 +10,10 @@ type Props = {
 export default function QueueHeader({ partyName, partyCode }: Props) {
   return (
     <div>
-      <div>
-        <Button text='go back' type='secondary' onClick={() => router.back()} />
+      <div className='container'>
+        {/* <Button text='go back' type='secondary' onClick={() => router.back()} /> */}
+        <p>Code: {partyCode}</p>
         <h1>{partyName}</h1>
-        <h2>Code: {partyCode}</h2>
       </div>
     </div>
   );
