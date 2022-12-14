e46f37a76fdf5130eaea43772eb61ead67abbaf6
Alexander Schuster <alexander@top11.at>
2022-12-14 23:28:53 +0100

    feat: JUKE-10 add endpoint for posting to playback queue

--- pages/api/queue/index.ts
+++ pages/api/queue/index.ts
@@ -16,14 +16,21 @@ async function handleGet(
   req: NextApiRequest,
   res: NextApiResponse<GetResponseBody>
 ) {
-
   const spotifyRes = await spotifyClient.get(`${BaseURL}`, {
     headers: {
       Authorization: req?.headers?.authorization,
     },
   });
   res.status(200).json(spotifyRes.data.queue);
+}
 
+async function handlePost(req: NextApiRequest, res: NextApiResponse<void>) {
+  await spotifyClient.post(`${BaseURL}?uri=${req?.query?.uri}`, {}, {
+    headers: {
+      Authorization: req?.headers?.authorization,
+    }
+  });
+  res.status(201);
 }
 
 export default async function handler(
@@ -33,5 +40,10 @@ export default async function handler(
   if (req.method === 'GET') {
     return await handleGet(req, res);
   }
+
+  if (req.method === 'POST') {
+    return await handlePost(req, res);
+  }
+
   return sendError(req, res, methodNotAllowed(['GET']));
 }
