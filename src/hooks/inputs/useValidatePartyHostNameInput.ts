9b3b979e2051887c62d9c68c6fa2cd5a43f5459a
Ramon Brullo <kingvanti650@gmail.com>
2022-12-15 12:55:43 +0100

    fix: incorrect project structure

--- src/hooks/inputs/useValidatePartyHostNameInput.ts
+++ src/hooks/inputs/useValidatePartyHostNameInput.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import * as z from 'zod';
-import nameInputSchema from '../../../utils/schemas/nameInputSchema';
+import nameInputSchema from '../../schemas/nameInputSchema';
 
 type UseValidatePartyHostNameInput = {
   partyHostName: string;
