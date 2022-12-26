1d5ca78d14cb2bd5d325eff8f8e7293be0a6711f
ComradeVanti <kingvanti650@gmail.com>
2022-12-26 12:51:03 +0100

    feat: add jukebox credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,11 @@
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PartyDb } from '@common/partyDb';
+import { PartyCode } from '@common/types/partyCode';
+import firebaseDb from '@common/firebaseDb';
+import { Party } from '@common/types/party';
+import { Guid } from 'guid-typescript';
 
 const Scope =
   'user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative';
@@ -15,6 +21,32 @@ function tryGetSpotifySecret(): string {
   if (secret !== undefined) return secret;
   else throw new Error('SPOTIFY_CLIENT_SECRET env not defined');
 }
+
+const jukeCredentialProvider = CredentialsProvider({
+  id: "Juke",
+  name: 'Juke',
+  credentials: {
+    partyCode: { label: 'Party-code', type: 'text' },
+    userId: { label: 'User-id', type: 'text' },
+  },
+  async authorize(credentials, req) {
+    if (credentials === undefined) return null;
+
+    const partyCode = PartyCode.tryMake(credentials.partyCode);
+    if (partyCode === null) return null;
+
+    const userId = Guid.isGuid(credentials.userId)
+      ? Guid.parse(credentials.userId)
+      : null;
+    if (userId === null) return null;
+
+    const party = await PartyDb.tryGetByCode(firebaseDb, partyCode);
+    if (PartyDb.isError(party)) return null;
+
+    if (!Party.hasUserWithId(party, userId)) return null;
+    return { id: userId.toString() };
+  },
+});
 
 export default NextAuth({
   providers: [
