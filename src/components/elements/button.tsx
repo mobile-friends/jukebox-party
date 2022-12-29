9bd0f6c38bbc8453d943fc0cf12abca3f61a5343
ComradeVanti <kingvanti650@gmail.com>
2022-12-29 17:23:53 +0100

    feat: add correct button async behaviour
    I think before, async click handlers would not be awaited. Now they should

--- src/components/elements/button.tsx
+++ src/components/elements/button.tsx
@@ -12,15 +12,14 @@ interface Props {
 }
 
 export default function Button({ text, type, icon, onClick }: Props) {
+  async function onClicked(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    await onClick(e);
+  }
+
   return (
     <div>
-      <button
-        className={`btn ${type}`}
-        onClick={(e) => {
-          e.preventDefault();
-          onClick(e);
-        }}
-      >
+      <button className={`btn ${type}`} onClick={onClicked}>
         {icon ? <div>{icon}</div> : ''}
         {text ? <div>{text}</div> : ''}
       </button>
