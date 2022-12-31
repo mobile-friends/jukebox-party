c5b338940ba2d05b96703d41ccf6188dd2e92a5f
ComradeVanti <kingvanti650@gmail.com>
2022-12-31 10:18:30 +0100

    doc: progress-bar
    Add js docs

--- src/components/elements/progressBar.tsx
+++ src/components/elements/progressBar.tsx
@@ -2,10 +2,20 @@ import styles from '../../styles/components/progressBar.module.scss';
 import { Duration } from '@common/types/duration';
 
 interface Props {
+  /**
+   * How long the song has been playing for
+   */
   progress: Duration;
+  /**
+   * The duration of the current track
+   */
   duration: Duration;
 }
 
+/**
+ * Displays the current playback progress
+ * @constructor
+ */
 export default function ProgressBar({ progress, duration }: Props) {
   const playPercent = (progress / duration) * 100;
   const fillerWidth = { width: `${playPercent}%` };
