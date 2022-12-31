b1e8645618702b451fa74dfdf9359960029bcb74
ComradeVanti <kingvanti650@gmail.com>
2022-12-31 11:28:18 +0100

    refactor: queue header
    clean up code

--- src/components/elements/queueHeader.tsx
+++ src/components/elements/queueHeader.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
 type Props = {
-  partyName: string | null;
-  partyCode: string | null;
+  partyName: string;
+  partyCode: string;
 };
 
 export default function QueueHeader({ partyName, partyCode }: Props) {
   return (
-    <div>
-      <div className='container'>
-        {/* <Button text='go back' type='secondary' onClick={() => router.back()} /> */}
-        <p>Code: {partyCode}</p>
-        <h1>{partyName}</h1>
-      </div>
+    <div className='container'>
+      <p>Code: {partyCode}</p>
+      <h1>{partyName}</h1>
     </div>
   );
 }
