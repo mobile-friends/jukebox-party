4fe65f1284446124dd0b67b53824cd3a27dc3a60
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-02 20:53:47 +0100

    refactor: juke-115 QrButton independent component

---
+++ src/components/elements/qrButton.tsx
@@ -0,0 +1,27 @@
+import Button from './button';
+import { HiOutlineQrCode } from 'react-icons/hi2';
+import QRCodeModal from '@component/qrCodeModal';
+import useToggle from '@hook/useToggle';
+import { PartyCode } from '@common/types/partyCode';
+
+interface Props {
+  partyCode: PartyCode;
+}
+
+export default function QrButton({ partyCode }: Props) {
+  const [isModalVisible, toggleModalVisibility] = useToggle();
+  
+  return (
+    <div>
+      <Button
+        styleType='icon-only small'
+        content={<HiOutlineQrCode />}
+        onClick={toggleModalVisibility}
+      />
+
+      {isModalVisible && (
+        <QRCodeModal onClosed={toggleModalVisibility} partyCode={partyCode} />
+      )}
+    </div>
+  );
+}
