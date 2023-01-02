77aa5c63ab58b3ae4c8bd9fc6f6c06b94e6db7a8
ComradeVanti <kingvanti650@gmail.com>
2023-01-02 13:45:22 +0100

    refactor: drop unused endpoint

--- src/pages/api/parties/[partyCode]/track.ts
+++
@@ -1,7 +0,0 @@
-import HTTPMethod from 'http-method-enum';
-import handleGetTrackRequest from '@endpoint/getPartyTrack';
-import { endpoint } from '@common/infrastructure/endpoint';
-
-export default endpoint({
-  [HTTPMethod.GET]: handleGetTrackRequest,
-});
