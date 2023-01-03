9e58911c9b1b7526a20a53b9c508829570a9a2b5
ComradeVanti <kingvanti650@gmail.com>
2023-01-03 16:50:14 +0100

    fix: useless log

--- src/common/util/assertions.ts
+++ src/common/util/assertions.ts
@@ -1,5 +1,7 @@
 // noinspection JSUnusedLocalSymbols
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function assertNeverReached(x: never): never {
-  throw new Error(`This should never be reached. Value was ${x}`);
+  throw new Error(
+    `This should never be reached. Value was ${JSON.stringify(x)}`
+  );
 }
