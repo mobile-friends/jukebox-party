35697ee6b1daffa5e05ebc3c1b43ae09ec834d2c
lukasganster <lukasganster13@gmail.com>
2023-01-03 19:54:16 +0100

    feat: add head and title

--- src/components/elements/jukeHeader.tsx
+++ src/components/elements/jukeHeader.tsx
@@ -1,12 +1,20 @@
+import Head from 'next/head';
+
 interface Props {
   first: string;
   second: string;
+  pageTitle?: string;
 }
 
-export default function JukeHeader({ first, second }: Props) {
+export default function JukeHeader({ first, second, pageTitle }: Props) {
   return (
-    <h1 className='text-center'>
-      {first}.<span className='text-primary text-italic'>{second}</span>
-    </h1>
+    <>
+      <Head>
+        <title>{pageTitle || 'jukebox.party'}</title>
+      </Head>
+      <h1 className='text-center'>
+        {first}.<span className='text-primary text-italic'>{second}</span>
+      </h1>
+    </>
   );
 }
