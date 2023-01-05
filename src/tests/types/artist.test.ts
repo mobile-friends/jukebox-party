0147c82e3414821ad6ee03ec09f9a600a5ca6b01
Eva <evamaria.altenburger@gmail.com>
2023-01-05 16:32:53 +0100

    fix: artist id test

--- src/tests/types/artist.test.ts
+++ src/tests/types/artist.test.ts
@@ -2,6 +2,8 @@ import { Artist } from '@common/types/artist';
 
 test('Created artists have the given name', () => {
   const name = 'Mr. music';
-  const artist = Artist.make(name);
+  const id ="asdf7890"
+  const artist = Artist.make(name, id);
   expect(Artist.nameOf(artist)).toBe(name);
+  expect(Artist.idOf(artist)).toBe(id);
 });
