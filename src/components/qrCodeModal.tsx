26582d3cff01626717db568b96b7e3ab52618831
Michelle Markl <65341595+MMischi@users.noreply.github.com>
2023-01-05 11:01:59 +0100

    refactor: qr modal for better usability
    Before every click closed the modal, this is bad usability. A close button was added, and a click on the background closes the modal.

--- src/components/qrCodeModal.tsx
+++ src/components/qrCodeModal.tsx
@@ -2,6 +2,8 @@ import QRCode from 'react-qr-code';
 import styles from '@style/components/qrCodeModal.module.scss';
 import { PartyCode } from '@common/types/partyCode';
 import { useLocation } from '@hook/useLocation';
+import { BaseSyntheticEvent } from 'react';
+import Button from './elements/button';
 
 type ModalCloseListener = () => void;
 
@@ -26,16 +28,37 @@ export default function QRCodeModal({ partyCode, onClosed }: Props) {
   // (auf localhost funktioniert es)
   const link = `${origin}/?partyCode=${partyCode}`;
 
+  function handleClick(e: BaseSyntheticEvent) {
+    const targetClassName: string = e.target.className;
+    if (
+      typeof targetClassName === 'string' &&
+      targetClassName.includes('background_clickable')
+    ) {
+      onClosed();
+    }
+  }
+
   return (
-    <div className={styles.background} onClick={onClosed}>
+    <div className={styles.background_clickable} onClick={handleClick}>
       <div className={styles.modal}>
         <h2 className='text-center'>Scan the party code</h2>
-        <div className={styles.QRCodeContainer}>
-          <QRCode value={link} />
+
+        <div className={styles.content}>
+          <div className={styles.QRCodeContainer}>
+            <QRCode value={link} />
+          </div>
+          <p className='text-center'>
+            Direct code to the party, you only have to enter your name then
+          </p>
         </div>
-        <p className='text-center'>
-          Direct code to the party, you only have to enter your name then
-        </p>
+
+        <div className={styles.closebar}>
+          <Button
+            styleType='secondary block'
+            content='Close'
+            onClick={onClosed}
+          />
+        </div>
       </div>
     </div>
   );
