3c5ba1b77fca5c911282a9eade529b79bbffcf82
lukasganster <lukasganster13@gmail.com>
2023-01-06 09:14:46 +0100

    refactor: lyricsWrapper styling

--- src/components/lyricsWrapper.tsx
+++ src/components/lyricsWrapper.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Duration } from '@common/types/duration';
 import { Track } from '@common/types/track';
-
+import styles from '@style/components/lyricsWrapper.module.scss';
 interface Props {
   playTime: Duration;
   track: Track;
@@ -22,42 +22,48 @@ export default function LyricsWrapper({ playTime, track }: Props) {
         return res.json();
       })
       .then((data) => {
-        const lyrics = data?.lines?.map((line: unknown) => {
+        const lyrics = data?.lines?.map((line: any) => {
           return {
             startTime: parseInt(line.startTimeMs) / 1000,
             words: line.words,
           };
         });
         setLyrics(lyrics);
-        console.log(lyrics);
-      });
+      })
+      .catch(() => setLyrics(null));
   }, [track, trackId]);
 
   useEffect(() => {
-    if (!lyrics || lyrics?.error) return;
-    const line = lyrics.reduce((prev: any, curr: any) => {
+    if (!lyrics) {
+      setCurrentLine('No lyrics found');
+      return;
+    }
+    const currentLine = lyrics.reduce((prev: any, curr: any) => {
       return Math.abs(curr.startTime - playTime) <
         Math.abs(prev.startTime - playTime)
         ? curr
         : prev;
     });
-    const prevLine = lyrics[lyrics.indexOf(line) - 1]?.words;
-    const nextLine = lyrics[lyrics.indexOf(line) + 1]?.words;
-    setCurrentLine(line.words);
+    const prevLine = lyrics[lyrics.indexOf(currentLine) - 1]?.words;
+    const nextLine = lyrics[lyrics.indexOf(currentLine) + 1]?.words;
+    setCurrentLine(currentLine.words);
     setPrevLine(prevLine);
     setNextLine(nextLine);
-    console.log({
-      prevLine,
-      currentLine: line.words,
-      nextLine,
-    });
-  }, [playTime]);
+  }, [playTime, lyrics]);
 
-  return (
-    <div className='lyrics-wrapper'>
-      <div className='lyrics-line'>{prevLine}</div>
-      <div className='lyrics-line current'>{currentLine}</div>
-      <div className='lyrics-line'>{nextLine}</div>
-    </div>
+  return lyrics ? (
+    <>
+      <div className={styles.lyricsWrapper}>
+        <div className={styles.lyricsLine}>{prevLine}</div>
+        <div className={styles.lyricsLineCurrent}>{currentLine}</div>
+        <div className={styles.lyricsLine}>{nextLine}</div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className={styles.lyricsNotFound}>
+        <div>No lyrics found for this song.</div>
+      </div>
+    </>
   );
 }
