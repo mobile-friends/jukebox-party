a551c46a2194fdd8ee2168069e4faf37a3d6a308
lukasganster <lukasganster13@gmail.com>
2023-01-07 13:36:49 +0100

    feat: JUKE-26 extend Artist with optional param

--- src/common/types/artist.ts
+++ src/common/types/artist.ts
@@ -7,6 +7,7 @@ declare const tag: unique symbol;
 export interface Artist {
   readonly name: string;
   readonly id: string;
+  readonly genres?: string[];
   readonly [tag]: 'Artist';
 }
 
@@ -18,8 +19,8 @@ export namespace Artist {
    * Makes an artist object
    * @param name The artists name
    */
-  export function make(name: string, id: string): Artist {
-    return Object.freeze({ name, id } as Artist);
+  export function make(name: string, id: string, genres?: string[]): Artist {
+    return Object.freeze({ name, id, genres } as Artist);
   }
 
   /**
@@ -30,12 +31,19 @@ export namespace Artist {
     if (Array.isArray(artist)) return artist.map((a) => a.name).join(', ');
     return artist.name;
   }
-    /**
+  /**
    * Gets the id of an artist
    * @param artist The artist
    */
-    export function idOf(artist: Artist): string {
-      if (Array.isArray(artist)) return artist.map((a) => a.id).join(', ');
-      return artist.id;
-    }
+  export function idOf(artist: Artist): string {
+    return artist.id;
+  }
+
+  /**
+   * Gets the genres of an artist
+   * @param artist The artist
+   */
+  export function genresOf(artist: Artist): string[] {
+    return artist.genres || [];
+  }
 }
