0f029108c0aa5c2000ae80af9ec2914f1ac49cfd
Eva <evamaria.altenburger@gmail.com>
2023-01-11 09:59:41 +0100

    feat: juke-165 added aboutus styling

--- src/components/aboutUsModal.tsx
+++ src/components/aboutUsModal.tsx
@@ -1,4 +1,4 @@
-import styles from '@style/components/userListModal.module.scss';
+import styles from '@style/components/aboutUsModal.module.scss';
 import Button from './elements/button';
 import { BaseSyntheticEvent } from 'react';
 
@@ -10,9 +10,11 @@ interface Props {
 
 export default function AboutUsModal({ onClosed }: Props) {
   function handleClick(e: BaseSyntheticEvent) {
-
     const targetClassName: string = e.target.className;
-    if(typeof(targetClassName) === 'string' && targetClassName.includes('background_clickable')) {
+    if (
+      typeof targetClassName === 'string' &&
+      targetClassName.includes('background_clickable')
+    ) {
       onClosed();
     }
   }
@@ -22,12 +24,26 @@ export default function AboutUsModal({ onClosed }: Props) {
       <div className={styles.modal}>
         <h2 className='text-center'>
           The party.<span className='text-italic text-primary'>people</span>
+          <p>that created this app</p>
         </h2>
 
-        <p>Test</p>
+        <div>
+          <p>
+            Jukebox.party was created during a project at the University of
+            Applied Sciences St. Pölten.
+          </p>
+          <p>
+            Within one semester seven students came up with the idea and
+            implemented it with Next.js.
+          </p>
+        </div>
 
-        <div className={styles.closebar}>
-          <Button styleType='secondary block' content='Close' onClick={onClosed} />
+        <div>
+          <Button
+            styleType='secondary block'
+            content='Close'
+            onClick={onClosed}
+          />
         </div>
       </div>
     </div>
