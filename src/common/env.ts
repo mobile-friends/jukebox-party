902884f57ac56933347c2dcdc55f1cd7eb50a5ca
lukasganster <lukasganster13@gmail.com>
2023-01-13 01:42:27 +0100

    fix: remove log

--- src/common/env.ts
+++ src/common/env.ts
@@ -41,7 +41,6 @@ export namespace Env {
 
   export function isProduction(): boolean {
     // TODO dynamisch mit ENV-Variablen machen - Funktioniert irgendwie nicht im Frontend
-    console.log('isProduction? ' + process.env.NEXT_PUBLIC_ENVIRONMENT);
     return process.env.NEXT_PUBLIC_ENVIRONMENT === 'production';
   }
 }
