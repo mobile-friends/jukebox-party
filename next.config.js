b8338ff32b7b35745d89fce865a2b6bb109de3d5
FruitDude222 <fabian.gaugusch@gmx.at>
2023-01-13 15:17:37 +0100

    updated next config for PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+  },
   reactStrictMode: true,
-};
+});
