97a86e8ec90107177f23d884609b8500efcfedb7
lukasganster <lukasganster13@gmail.com>
2023-01-13 02:46:32 +0100

    fix: prevent auto-zoom on mobile

--- src/components/elements/jukeHeader.tsx
+++ src/components/elements/jukeHeader.tsx
@@ -11,6 +11,10 @@ export default function JukeHeader({ first, second, pageTitle }: Props) {
     <>
       <Head>
         <title>{pageTitle || 'jukebox.party'}</title>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        ></meta>
       </Head>
       <h1 className='text-center'>
         {first}.<span className='text-primary text-italic'>{second}</span>
