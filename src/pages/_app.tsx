c0df12f549ddc19f02cc5f640abdb8ed60af52ec
lukasganster <lukasganster13@gmail.com>
2023-01-15 16:46:16 +0100

    feat: JUKE-172 add favicon for all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,10 @@ export default function MyApp({
         <title>jukebox.party</title>
         <meta name='description' content='Jukebox party' />
         <link rel='manifest' href='/manifest.json' />
+        <link
+          rel='icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🎉</text></svg>'
+        ></link>
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
